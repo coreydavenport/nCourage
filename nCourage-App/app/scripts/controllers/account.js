49f9ac1f515b734aee385322948017ca2de910ee
Corey Davenport <cairnelan@gmail.com>
2016-01-18 19:39:02 -0500

    Removed unnecessary change email/password fields

--- nCourage-App/app/scripts/controllers/account.js
+++ nCourage-App/app/scripts/controllers/account.js
@@ -15,32 +15,9 @@ angular.module('nCourageApp')
     profile.$bindTo($scope, 'profile');
     
 
-    $scope.changePassword = function(oldPass, newPass, confirm) {
-      $scope.err = null;
-      if( !oldPass || !newPass ) {
-        error('Please enter all fields');
-      }
-      else if( newPass !== confirm ) {
-        error('Passwords do not match');
-      }
-      else {
-        Auth.$changePassword({email: profile.email, oldPassword: oldPass, newPassword: newPass})
-          .then(function() {
-            success('Password changed');
-          }, error);
-      }
-    };
+  
 
-    $scope.changeEmail = function(pass, newEmail) {
-      $scope.err = null;
-      Auth.$changeEmail({password: pass, newEmail: newEmail, oldEmail: profile.email})
-        .then(function() {
-          profile.email = newEmail;
-          profile.$save();
-          success('Email changed');
-        })
-        .catch(error);
-    };
+  
 
     function error(err) {
       alert(err, 'danger');
