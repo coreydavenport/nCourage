ecf11b3fbbc19e51874d0672a03adde5efe9e5f6
Corey Davenport <cairnelan@gmail.com>
2016-01-25 17:14:50 -0500

    Added firebase

--- nCourage-App/app/scripts/controllers/main.js
+++ nCourage-App/app/scripts/controllers/main.js
@@ -8,9 +8,19 @@
  * Controller of the nCourageApp
  */
 angular.module('nCourageApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, $http, $firebaseArray) {
+    
+    scope.user = new Object();
+
+    var URL = "https://ncourageapp.firebaseio.com/";
+    var list = $firebaseArray(new Firebase(URL));
+    $scope.todos = list;
+    console.log("Firebase Data", $scope.todos);
+
     $scope.addToDo = function () {
+      
+      console.log("form data", $scope.todo);
+
       $scope.todos.push($scope.todo);
       $scope.todo = '';
     };
